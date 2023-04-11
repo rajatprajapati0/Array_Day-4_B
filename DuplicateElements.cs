ecc682a0b9f665782b3b869bb730239e0d8b7560
Rajat <rajatprajapati09@gmail.com>
2023-04-12 04:06:23 +0530

    counting total number of duplicate elements in an array

---
+++ DuplicateElements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/*1) Write a program in C# Sharp to count a total number of duplicate elements
+in an array.*/
+namespace ArrayExample
+{
+    internal class DuplicateElements
+    {
+        public static void duplicateElements()
+        {
+            int[] arry = {1,2, 3, 0, 5,5,2};
+            int count=0;
+            int i,j;
+    
+
+            for ( i = 0; i < arry.Length; i++)
+            {
+
+                for (j = i+1; j < arry.Length; j++) 
+                {
+
+                    if (arry[i] == arry[j]) 
+                    {
+                        count++;
+                        break;
+                    }
+                
+                }
+            
+            }
+            Console.WriteLine($"total number of duplicate elements in an array{count}");
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ArrayExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DuplicateElements.duplicateElements();
+        }
+    }
+}
